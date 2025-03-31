91f443980b137d526e2af4f381427ae24a4444d8
Ujjwal Kumar Singh <163384387+Ujjwalks9@users.noreply.github.com>
2025-04-01 00:28:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 class Config:
-<<<<<<< HEAD
+
     SECRET_KEY = "your-secret-key"  
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Anjudk92@localhost/IIITK"
-=======
-    SECRET_KEY = "your_secret_key"  
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:97082@localhost/IIITK"
->>>>>>> e542fb22b4fc97aa95d94e125a5ce237175610ea
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:your-sql-password@localhost/IIITK"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
